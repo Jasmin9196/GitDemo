8547bc877c564cbcb1e06c7bce73b53688e763e4
jasminp <pateljasmin8578.jp@gmail.com>
2024-03-09 12:21:17 +0530

    first commit

---
+++ src/Sellintroduction.java
@@ -0,0 +1,12 @@
+
+public class Sellintroduction {
+	
+	public static void main (String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver_win32.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+	}
+
+}
